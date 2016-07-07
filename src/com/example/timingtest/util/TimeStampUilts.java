aa6927918208badde544391cca4c9221bd2de231
sunran <sunran1992@tom.com>
2016-07-07 10:59:48 +0800

    加入HashMap用于存储调用前的<函数名-时间>对，并将结果保存在SD卡中，文件为r.txt

--- src/com/example/timingtest/util/TimeStampUilts.java
+++ src/com/example/timingtest/util/TimeStampUilts.java
@@ -3,6 +3,8 @@ package com.example.timingtest.util;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import org.apache.http.impl.conn.Wire;
 
@@ -14,12 +16,11 @@ public class TimeStampUilts {
 	private static final String TAG = "TimeStamp";
 
 	private static long sTimeStampBefore;
-	private static String sApiNameBefore;
 	private static long sTimeStampAfter;
-	private static String sApiNameAfter;
 	private final static String FILE_NAME = "r.txt";
 	private static File targetFile;
 	private static RandomAccessFile raf;
+	private static HashMap<String,Long> apiBeforeStampMap = new HashMap<String,Long>(); 
 
 	public static void init() {
 		try {
@@ -33,27 +34,29 @@ public class TimeStampUilts {
 
 	public static long stampBeforeApi(String apiName) {
 		sTimeStampBefore = System.nanoTime();
+		apiBeforeStampMap.put(apiName, sTimeStampBefore);
 		Log.d(TAG, apiName + "前：\t" + sTimeStampBefore);
-		sApiNameBefore = apiName;
 		return sTimeStampBefore;
 	}
 
 	public static long stampAfterApi(String apiName) {
 		sTimeStampAfter = System.nanoTime();
 		Log.d(TAG, apiName + "后：\t" + sTimeStampAfter);
-		sApiNameAfter = apiName;
-		calTimeDiff();
+		long diff = calTimeDiff(apiName,sTimeStampAfter);
+		if(diff==0)return 0;
+		else writeToFile(apiName+":"+diff+"\n");
 		return sTimeStampAfter;
 	}
 
-	public static long calTimeDiff() {
-		if (sApiNameAfter.equals(sApiNameBefore)) {
-			long diff = sTimeStampAfter - sTimeStampBefore;
-			Log.d(TAG, sApiNameAfter + "时间差：\t" + diff);
-			writeToFile(sApiNameAfter+":"+diff+"\n");
+	public static long calTimeDiff(String apiName,long timeAfter) {
+		if(!apiBeforeStampMap.containsKey(apiName)){
+			return 0;
+		} else {
+			long timeBefore = apiBeforeStampMap.get(apiName);
+			long diff = timeAfter - timeBefore;
+			apiBeforeStampMap.remove(apiName);
 			return diff;
 		}
-		return 0;
 	}
 
 	private static void writeToFile(String s) {
