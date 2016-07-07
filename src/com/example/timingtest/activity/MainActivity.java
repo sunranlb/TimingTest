898561e672a3428b65e722cba52c5e21fbc18f3a
sunran <sunran1992@tom.com>
2016-07-07 10:28:23 +0800

    [+]将结果以文件形式写入SD卡

--- src/com/example/timingtest/activity/MainActivity.java
+++ src/com/example/timingtest/activity/MainActivity.java
@@ -8,7 +8,9 @@ import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -29,9 +31,21 @@ public class MainActivity extends Activity{
 		setContentView(R.layout.activity_main);
 		tv1 = (TextView) findViewById(R.id.tv1);
 		btn1 = (Button) findViewById(R.id.btn1);
+		
+		//is SDcard exists???
+		if(isSdCardExist()){
+			tv1.setText("SDcard path:"+getSdCardPath());
+		} else {
+			tv1.setText("SDcard not found!!!");
+			tv1.setTextColor(Color.RED);
+			btn1.setClickable(false);
+		}
+		
 		btn1.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				TimeStampUilts.init();
+				
 				Intent intent = new Intent(MainActivity.this, SecondActivity.class);
 				TimeStampUilts.stampBeforeApi("startActivity");
 				startActivity(intent);
@@ -41,6 +55,32 @@ public class MainActivity extends Activity{
 		
 	}
 
+	/** 
+	 * 判断SDCard是否存在 [当没有外挂SD卡时，内置ROM也被识别为存在sd卡] 
+	 *  
+	 * @return 
+	 */  
+	public boolean isSdCardExist() {  
+	    return Environment.getExternalStorageState().equals(  
+	            Environment.MEDIA_MOUNTED);  
+	} 
 	
+	/** 
+	 * 获取SD卡根目录路径 
+	 *  
+	 * @return 
+	 */  
+	public String getSdCardPath() {  
+	    boolean exist = isSdCardExist();  
+	    String sdpath = "";  
+	    if (exist) {  
+	        sdpath = Environment.getExternalStorageDirectory()  
+	                .getAbsolutePath();  
+	    } else {  
+	        sdpath = "不适用";  
+	    }  
+	    return sdpath;  
+	  
+	}  
 
 }
--- src/com/example/timingtest/util/TimeStampUilts.java
+++ src/com/example/timingtest/util/TimeStampUilts.java
@@ -1,23 +1,43 @@
 package com.example.timingtest.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import org.apache.http.impl.conn.Wire;
+
+import android.os.Environment;
 import android.util.Log;
 
 public class TimeStampUilts {
 
 	private static final String TAG = "TimeStamp";
-	
+
 	private static long sTimeStampBefore;
 	private static String sApiNameBefore;
 	private static long sTimeStampAfter;
 	private static String sApiNameAfter;
-	
+	private final static String FILE_NAME = "r.txt";
+	private static File targetFile;
+	private static RandomAccessFile raf;
+
+	public static void init() {
+		try {
+			targetFile = new File(Environment.getExternalStorageDirectory()
+					.getCanonicalPath() + "/"+FILE_NAME);
+			raf = new RandomAccessFile(targetFile, "rw");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static long stampBeforeApi(String apiName) {
 		sTimeStampBefore = System.nanoTime();
 		Log.d(TAG, apiName + "前：\t" + sTimeStampBefore);
 		sApiNameBefore = apiName;
 		return sTimeStampBefore;
 	}
-	
+
 	public static long stampAfterApi(String apiName) {
 		sTimeStampAfter = System.nanoTime();
 		Log.d(TAG, apiName + "后：\t" + sTimeStampAfter);
@@ -25,15 +45,24 @@ public class TimeStampUilts {
 		calTimeDiff();
 		return sTimeStampAfter;
 	}
-	
+
 	public static long calTimeDiff() {
 		if (sApiNameAfter.equals(sApiNameBefore)) {
 			long diff = sTimeStampAfter - sTimeStampBefore;
 			Log.d(TAG, sApiNameAfter + "时间差：\t" + diff);
+			writeToFile(sApiNameAfter+":"+diff+"\n");
 			return diff;
 		}
 		return 0;
 	}
-	
-	
+
+	private static void writeToFile(String s) {
+		try {
+			raf.seek(targetFile.length());
+			raf.write(s.getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
